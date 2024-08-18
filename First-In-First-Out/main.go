6f9bd2028187383019ea32b2adf31348cb1d20b9
Helio Loureiro <helio@loureiro.eng.br>
2024-08-18 18:17:23 +0200

    Adding the FIFO in a simple way

--- First-In-First-Out/main.go
+++ First-In-First-Out/main.go
@@ -1,328 +1,97 @@
 package main
 
-// copied from https://github.com/scalalang2/golang-fifo/blob/main/s3fifo/s3fifo.go
+// src: https://girai.dev/blog/lru-cache-implementation-in-go/
 
 import (
-	"container/list"
-	"context"
 	"fmt"
-	"sync"
-	"time"
 )
 
 func main() {
 	fmt.Println("Implementing simple FIFO caching")
 }
 
-const numberOfShards = 100
-
-// entry holds the key and value of a cache entry.
-type entry[K comparable, V any] struct {
-	key       K
-	value     V
-	freq      byte
-	element   *list.Element
-	expiredAt time.Time
-	shardID   int8 // shared wehere an index which the entry is stored within the shard
+type FIFOCache struct {
+	size  int               // capacity
+	table map[string]string // hash table for list.List.Element existence check
+	index []string
 }
 
-// shard is a container holding entries to be expired
-// ref. hashicorp/golang-lru
-type shard[K comparable, V any] struct {
-	entries     map[K]*entry[K, V]
-	newestEntry time.Time
+// Constructor initializes a new FIFOCache
+func New(capacity int) FIFOCache {
+	return FIFOCache{
+		size:  capacity,
+		table: make(map[string]string),
+		index: make([]string, 0),
+	}
 }
 
-type S3FIFO[K comparable, V any] struct {
-	ctx    context.Context
-	cancel context.CancelFunc
-	mu     sync.Mutex
-
-	// size is the maximum number of entries in the cache.
-	size int
-
-	// followings are the fundamental data structures of S3FIFO algorithm.
-	items map[K]*entry[K, V]
-	small *list.List
-	main  *list.List
-	ghost *ghost[K]
-
-	shards []shard[K, V]
-
-	// ttl is the time to live of the cache entry
-	ttl time.Duration
-
-	// an index of the next shard to be cleaned up
-	nextCleanupShard int8
-
-	// callback is the function that will be called when an entry is evicted from the cache
-	callback OnEvictCallback[K, V]
+// Get a list node from the hash map.
+func (cache *FIFOCache) Get(key string) string {
+	// check if list node exists
+	if value, ok := cache.table[key]; ok {
+		return value
+	}
+	return "not found"
 }
 
-var _ Cache[int, int] = (*S3FIFO[int, int])(nil)
-
-func New[K comparable, V any](size int, ttl time.Duration) *S3FIFO[K, V] {
-	ctx, cancel := context.WithCancel(context.Background())
-
-	if ttl <= 0 {
-		ttl = 0
+// Put key and value in the LRUCache
+func (cache *FIFOCache) Set(key, value string) {
+	if len(cache.table) != len(cache.index) {
+		fmt.Println("===== ERROR ====")
+		fmt.Println("cache:", cache.table)
+		fmt.Println("indexes:", cache.table)
+		panic("Found size and index mismatch!")
 	}
 
-	cache := &S3FIFO[K, V]{
-		ctx:              ctx,
-		cancel:           cancel,
-		size:             size,
-		items:            make(map[K]*entry[K, V]),
-		small:            list.New(),
-		main:             list.New(),
-		ghost:            newGhost[K](size),
-		shards:           make([]shard[K, V], numberOfShards),
-		ttl:              ttl,
-		nextCleanupShard: 0,
-	}
-
-	for i := 0; i < numberOfShards; i++ {
-		cache.shards[i].entries = make(map[K]*entry[K, V])
-	}
-
-	if ttl != 0 {
-		go func(ctx context.Context) {
-			ticker := time.NewTicker(ttl / numberOfShards)
-			defer ticker.Stop()
-			for {
-				select {
-				case <-ctx.Done():
-					return
-				case <-ticker.C:
-					cache.deleteExpired()
-				}
-			}
-		}(cache.ctx)
-	}
-
-	return cache
-}
-
-func (s *S3FIFO[K, V]) Set(key K, value V) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	if el, ok := s.items[key]; ok {
-		s.removeFromShard(el) // remove from the shard as the entry is updated
-		el.value = value
-		el.freq = min(el.freq+1, 3)
-		el.expiredAt = time.Now().Add(s.ttl)
-		s.addToShard(el)
-		return
-	}
-
-	for s.small.Len()+s.main.Len() >= s.size {
-		s.evict()
-	}
-
-	// create a new entry to append it to the cache.
-	ent := &entry[K, V]{
-		key:       key,
-		value:     value,
-		freq:      0,
-		expiredAt: time.Now().Add(s.ttl),
-	}
-
-	if s.ghost.contains(key) {
-		s.ghost.remove(key)
-		ent.element = s.main.PushFront(key)
+	// check if list node exists
+	if ok := cache.table[key]; ok != "" {
+		cache.MoveKeyToLast(key)
+		cache.Update(key, value)
 	} else {
-		ent.element = s.small.PushFront(key)
-	}
+		// here check what to do because key doesn't exist in cache
 
-	s.items[key] = ent
-	s.addToShard(ent)
-}
-
-func (s *S3FIFO[K, V]) Get(key K) (value V, ok bool) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	if _, ok := s.items[key]; !ok {
-		return value, false
-	}
-
-	s.items[key].freq = min(s.items[key].freq+1, 3)
-	s.ghost.remove(key)
-	return s.items[key].value, true
-}
-
-func (s *S3FIFO[K, V]) Remove(key K) (ok bool) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	if e, ok := s.items[key]; ok {
-		s.removeEntry(e)
-		return true
-	}
-
-	return false
-}
-
-func (s *S3FIFO[K, V]) Contains(key K) (ok bool) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	if _, ok := s.items[key]; ok {
-		return true
-	}
-	return false
-}
-
-func (s *S3FIFO[K, V]) Peek(key K) (value V, ok bool) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	el, ok := s.items[key]
-	if !ok {
-		return value, false
-	}
-	return el.value, ok
-}
-
-func (s *S3FIFO[K, V]) SetOnEvicted(callback OnEvictCallback[K, V]) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	s.callback = callback
-}
-
-func (s *S3FIFO[K, V]) Len() int {
-	return s.small.Len() + s.main.Len()
-}
-
-func (s *S3FIFO[K, V]) Purge() {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	for k := range s.items {
-		delete(s.items, k)
-	}
-
-	s.small.Init()
-	s.main.Init()
-	s.ghost.clear()
-}
-
-func (s *S3FIFO[K, V]) Close() {
-	s.Purge()
-	s.mu.Lock()
-	s.cancel()
-	s.mu.Unlock()
-}
-
-func (s *S3FIFO[K, V]) removeEntry(e *entry[K, V]) {
-	if s.callback != nil {
-		s.callback(e.key, e.value)
-	}
-
-	if s.ghost.contains(e.key) {
-		s.ghost.remove(e.key)
-	}
-
-	s.main.Remove(e.element)
-	s.small.Remove(e.element)
-	delete(s.items, e.key)
-}
-
-func (s *S3FIFO[K, V]) addToShard(e *entry[K, V]) {
-	if s.ttl == 0 {
-		return
-	}
-	shardId := (numberOfShards + s.nextCleanupShard - 1) % numberOfShards
-	e.shardID = shardId
-	s.shards[shardId].entries[e.key] = e
-	if s.shards[shardId].newestEntry.Before(e.expiredAt) {
-		s.shards[shardId].newestEntry = e.expiredAt
-	}
-}
-
-func (s *S3FIFO[K, V]) removeFromShard(e *entry[K, V]) {
-	if s.ttl == 0 {
-		return
-	}
-	delete(s.shards[e.shardID].entries, e.key)
-}
-
-func (s *S3FIFO[K, V]) deleteExpired() {
-	s.mu.Lock()
-
-	shardId := s.nextCleanupShard
-	s.nextCleanupShard = (s.nextCleanupShard + 1) % numberOfShards
-	shard := &s.shards[shardId]
-	timeToExpire := time.Until(shard.newestEntry)
-	if timeToExpire > 0 {
-		s.mu.Unlock()
-		time.Sleep(timeToExpire)
-		s.mu.Lock()
-	}
-
-	for _, e := range shard.entries {
-		s.removeEntry(e)
-	}
-
-	s.mu.Unlock()
-}
-
-func (s *S3FIFO[K, V]) evict() {
-	// if size of the small queue is greater than 10% of the total cache size.
-	// then, evict from the small queue
-	if s.small.Len() > s.size/10 {
-		s.evictFromSmall()
-		return
-	}
-	s.evictFromMain()
-}
-
-func (s *S3FIFO[K, V]) evictFromSmall() {
-	mainCacheSize := s.size / 10 * 9
-
-	evicted := false
-	for !evicted && s.small.Len() > 0 {
-		key := s.small.Back().Value.(K)
-		el, ok := s.items[key]
-		if !ok {
-			panic("s3fifo: entry not found in the cache")
-		}
-
-		if el.freq > 1 {
-			// move the entry from the small queue to the main queue
-			s.small.Remove(el.element)
-			s.items[key].element = s.main.PushFront(el.key)
-
-			if s.main.Len() > mainCacheSize {
-				s.evictFromMain()
-			}
+		// if full, delete the fist item
+		if cache.IsFull() {
+			// apply eviction
+			cache.EvictFirst()
+			cache.Add(key, value)
 		} else {
-			s.removeEntry(el)
-			s.ghost.add(key)
-			evicted = true
-			delete(s.items, key)
+			// not full
+			cache.Add(key, value)
 		}
 	}
 }
 
-func (s *S3FIFO[K, V]) evictFromMain() {
-	evicted := false
-	for !evicted && s.main.Len() > 0 {
-		key := s.main.Back().Value.(K)
-		el, ok := s.items[key]
-		if !ok {
-			panic("s3fifo: entry not found in the cache")
-		}
-
-		if el.freq > 0 {
-			s.main.Remove(el.element)
-			s.items[key].freq -= 1
-			s.items[key].element = s.main.PushFront(el.key)
-		} else {
-			s.removeEntry(el)
-			evicted = true
-			delete(s.items, key)
+func (cache *FIFOCache) MoveKeyToLast(key string) {
+	newOrdering := make([]string, 0)
+	for _, value := range cache.index {
+		if value != "" || value != key {
+			newOrdering = append(newOrdering, value)
 		}
 	}
+	newOrdering = append(newOrdering, key)
+	cache.index = newOrdering
+}
+
+func (cache *FIFOCache) Update(key, value string) {
+	cache.table[key] = value
+}
+
+func (cache *FIFOCache) EvictFirst() {
+	firstItem := cache.index[0]
+	rearranged := make([]string, 0)
+	for i := 1; i < cache.size; i++ {
+		rearranged = append(rearranged, cache.index[i])
+	}
+	delete(cache.table, firstItem)
+	cache.index = rearranged
+}
+
+func (cache *FIFOCache) Add(key, value string) {
+	cache.index = append(cache.index, key)
+	cache.table[key] = value
+}
+
+func (cache *FIFOCache) IsFull() bool {
+	return cache.size == len(cache.table)
 }
--- First-In-First-Out/main_test.go
+++ First-In-First-Out/main_test.go
@@ -1,8 +1,150 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestCreateCache(t *testing.T) {
-	cache := New[string, string](10, 0)
-	cache.Set("hello", "world")
+func TestCreateCacheSize2(t *testing.T) {
+	t.Log("Creating cache with size=2")
+	cache := New(2) // nil
+	t.Logf("cache data: %v", cache)
+	t.Log("Adding (1,10) to the cache")
+	cache.Set("1", "10") // nil, linked list: [1:10]
+	t.Logf("cache data: %v", cache)
+	t.Log("Adding (2,20) to the cache")
+	cache.Set("2", "20") // nil, linked list: [2:20, 1:10]
+	t.Logf("cache data: %v", cache)
+
+	resp := cache.Get("1")
+	if resp != "10" {
+		t.Errorf("Cache sent: %v - expected 10", resp)
+	}
+	t.Logf("cache data: %v", cache)
+
+	resp = cache.Get("2")
+	if resp != "20" {
+		t.Errorf("Cache sent: %v - expected 20", resp)
+	}
+	t.Logf("cache data: %v", cache)
+
+	for i := 1; i < 4; i++ {
+		t.Logf("Cache(%v):%v", i, cache.Get(fmt.Sprintf("%d", i)))
+	}
+
+	t.Log("Adding (3,30) to the cache  - it will replace (1, 10)")
+	cache.Set("3", "30") // nil, linked list: [3:30, 1:10]
+	t.Logf("cache data: %v", cache)
+
+	for i := 1; i < 4; i++ {
+		t.Logf("Cache(%v):%v", i, cache.Get(fmt.Sprintf("%d", i)))
+	}
+	t.Logf("cache data: %v", cache)
+
+	resp = cache.Get("1")
+	if resp != "not found" {
+		t.Errorf("Cache sent: %v - expected \"not found\"", resp)
+	}
+
+	resp = cache.Get("2")
+	if resp != "20" {
+		t.Errorf("Cache sent: %v - expected 20", resp)
+	}
+
+	resp = cache.Get("3")
+	if resp != "30" {
+		t.Errorf("Cache sent: %v - expected 30", resp)
+	}
+
+	t.Logf("cache data: %v", cache)
+
+	t.Log("Adding (4,40) to the cache  - it will replace (2, 20)")
+	cache.Set("4", "40")
+	t.Logf("cache data: %v", cache)
+
+	resp = cache.Get("1")
+	if resp != "not found" {
+		t.Errorf("Cache sent: %v - expected \"not found\"", resp)
+	}
+	resp = cache.Get("2")
+	if resp != "not found" {
+		t.Errorf("Cache sent: %v - expected \"not found\"", resp)
+	}
+
+	resp = cache.Get("3")
+	if resp != "30" {
+		t.Errorf("Cache sent: %v - expected 30", resp)
+	}
+	resp = cache.Get("4")
+	if resp != "40" {
+		t.Errorf("Cache sent: %v - expected 40", resp)
+	}
+}
+
+func TestCreateCacheSize10(t *testing.T) {
+	// t.SkipNow()
+	t.Log("Creating cache with size=10")
+	cache := New(10)
+	t.Log("Adding (1,10) to the cache")
+	cache.Set("1", "10")
+	t.Log("Adding (2,20) to the cache")
+	cache.Set("2", "20")
+	t.Log("Adding (3,30) to the cache")
+	cache.Set("3", "30")
+	t.Log("Adding (4,40) to the cache")
+	cache.Set("4", "40")
+	t.Log("Adding (5,50) to the cache")
+	cache.Set("5", "50")
+	t.Log("Adding (6,60) to the cache")
+	cache.Set("6", "60")
+	t.Log("Adding (7,70) to the cache")
+	cache.Set("7", "70")
+	t.Log("Adding (8,80) to the cache")
+	cache.Set("8", "80")
+	t.Log("Adding (9,90) to the cache")
+	cache.Set("9", "90")
+	t.Log("Adding (10,100) to the cache")
+	cache.Set("10", "100")
+
+	resp := cache.Get("1")
+	if resp != "10" {
+		t.Errorf("Cache sent: %v - expected 10", resp)
+	}
+
+	resp = cache.Get("5")
+	if resp != "50" {
+		t.Errorf("Cache sent: %v - expected 50", resp)
+	}
+
+	t.Log("Adding (11,110) to the cache - replacing (1, 10)")
+	cache.Set("11", "110")
+	resp = cache.Get("10")
+	if resp != "100" {
+		t.Errorf("Cache sent: %v - expected 100", resp)
+	}
+	resp = cache.Get("11")
+	if resp != "110" {
+		t.Errorf("Cache sent: %v - expected 110", resp)
+	}
+	if cache.index[0] != "2" {
+		t.Errorf("Expected index \"11\" but received: %v", cache.index[0])
+	}
+
+	for i := 1; i < 12; i++ {
+		t.Logf("Cache(%v):%v", i, cache.Get(fmt.Sprintf("%d", i)))
+	}
+	// resp = cache.Get(1)
+	// if resp != -1 {
+	// 	t.Errorf("Cache sent: %v - expected -1", resp)
+	// }
+
+	t.Log("Adding (12,120) to the cache - replacing (2, 20)")
+	cache.Set("12", "120")
+	for i := 1; i < 12; i++ {
+		t.Logf("Cache(%v):%v", i, cache.Get(fmt.Sprintf("%d", i)))
+	}
+	if cache.index[0] != "3" {
+		t.Errorf("Expected index \"11\" but received: %v", cache.index[0])
+	}
+
 }
